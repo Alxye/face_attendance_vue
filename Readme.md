a472f0187bd7b42e255b08a16620c8df1a0ec753
Xixi <944652593zxx@gmail.com>
2023-03-16 23:07:03 +0800

    03 16 night

--- Readme.md
+++ Readme.md
@@ -1,15 +1,21 @@
 # 智慧考勤管理 - 后台管理系统
+
 用户验证、公司信息管理、部门管理  
 负责人：赵茜茜
+
 ## 2023.3.13 zxx
+
 * 确认使用vue3+Ts开发web前端，传递json数据与后端flask程序交互
 * 完成管理员登录验证与token存取，store.modules.user下存取管理员token与登录信息
 * 完成管理员密码修改功能
 * 修改store.user.info结构，并入存储用户非敏感信息
 * 设计主页的布局格式
+
 ## 2023.3.14 zxx
+
 * 设计弹窗功能，新增width参数调整弹窗大小自适应`src/components/layer/index.vue`
-> `src/layout/Header/passwordLayer.vue` 
+
+> `src/layout/Header/passwordLayer.vue`
 > ```  
 > const layer = reactive({
 >    width:"30%",
@@ -17,46 +23,57 @@
 >    showButton: true
 > }) 
 > ```
+
 * 将公司信息存储至store，配合vuex
 * 完成了公司info卡片的展示以及公司信息的修正，新增`src/api/corporation.ts`用于公司模块request请求
 * 设计loading动画，当获取服务器端数据时load页面，超时则404，代码：`src/utils/system/loading.ts`<br/>
-具体用法：
+  具体用法：
+
 ```angular2html
 > ...vue & ts
 <script>
-    import { showLoading, hideLoading } from '@/utils/system/loading'
+    import {showLoading, hideLoading} from '@/utils/system/loading'
+
     ...
     showLoading()
     ...
     hideLoading()
 </script>
 ```
+
 | 待解决：pieChart传值问题，初始chart值为const，端口传值类型相同但...@_@
+
 ## 2023.3.15
+
 * pieChart传值问题解决了，在后端接口返回json的时候，前端处理成array即可
 * pieChart新问题：request请求好了之后，组件内部data没有变，sad（没有直接的解决方式），原有的option本就是常量，option已经是Porxy类型了，但是没有用，emo了
 * pieChart解决方法：干脆简单粗暴，request部门分布信息存入store中，然后每次request的时候判断一下信息有没有变，变了则reload
 * Big Problem：request请求后，页面数据一点没变 <br/> Solution：引入reactive()就能解决了，瑞斯拜
 * `首页做完哩！`
 * 开始做公司信息管理了，设计思路
+
 ````angular2html
 -信息管理
-    -部门信息管理
-        -新增部门
-        -删除已有部门
-        -修改部门信息
-        -删除已有部门（部门无人则删除，有人则不能删）
-        -修改部门信息（部门名称、部门公告）
-    -打卡时间设置
+-部门信息管理
+-新增部门
+-删除已有部门
+-修改部门信息
+-删除已有部门（部门无人则删除，有人则不能删）
+-修改部门信息（部门名称、部门公告）
+-打卡时间设置
 ````
-* 设计了部门信息管理初步框架，待完善功能：修改部门信息，待添加功能：删除已有部门/新增部门 
+
+* 设计了部门信息管理初步框架，待完善功能：修改部门信息，待添加功能：删除已有部门/新增部门
+
 ## 2023.3.16
+
 * 今日组会，不多搞了
 * 新增部门有了，但是还要加不可增加已存在部门，就酱
 * 大成功！！！新增部门+更新部门信息全部完成，还弄了一个卡片组件与父子传值与监听变更 WIN
 * 部门信息管理写完了，在考虑要不要将打卡时间设计也放部门信息管理里面
 * 今日代码：卡片组件与传值<br/>
-` 组件应用src/views/main/department/manage.vue`
+  ` 组件应用src/views/main/department/manage.vue`
+
 ```angular2html
 <template>
 <el-tree
@@ -72,7 +89,8 @@
     // 引入子组件 用于展示选中列表项的部门信息
     components: {
         MangeInfo
-    },
+    }
+    ,
     // 卡片内容随列表选中变更
     const handleNodeClick = (row: object) => {
         layer.show = true
@@ -80,7 +98,34 @@
         console.log("row:", row);
     }
 ```
-`组件设计：src/views/main/department/mangeInfo.vue`
-```angular2html
 
+`组件设计：src/views/main/department/mangeInfo.vue`
+
+```angular2html
+字典
+<script>
+    props: {
+        dic: {
+            type: Object,
+        default:
+            () => {
+                return {
+                    name: '',
+                    notice: '',
+                    staff_count: 0,
+                    id: 0
+                }
+            }
+        }
+    ,
+    }
+    **
+    数据监听 **
+    watch(
+            () => props.dic,
+            (newValue, oldValue) => {
+            ...
+            }
+    )
 ```
+* 加入了打卡时间设置，但有一个问题，new Data().setTime(时间戳)返回的是number对象，而不是Data对象无法在TimePicker里面显示（悲
--- src/views/main/department/mangeInfo.vue
+++ src/views/main/department/mangeInfo.vue
@@ -10,20 +10,36 @@
       <el-form-item label="部门人数：" prop="staff_count">
         <span>{{ form.staff_count }} 人</span>
       </el-form-item>
+      <el-form-item label="上班打卡时间段：" prop="clock_in">
+        <el-time-picker
+            @change="TimePicker()"
+            v-model="form.clockin"
+            is-range
+            range-separator="To"
+            start-placeholder="Start time"
+            end-placeholder="End time"
+        />
+      </el-form-item>
+      <el-form-item label="下班打卡时间段：" prop="clock_out">
+        <el-time-picker
+            v-model="form.clockout"
+            is-range
+            range-separator="To"
+            start-placeholder="Start time"
+            end-placeholder="End time"
+        />
+      </el-form-item>
+      <div class="wrapper">
+
+      </div>
+      <el-col class="wrapper">
+        <el-button class="wrapper-content" type="primary" @click="update">确认</el-button>
+        <el-button class="wrapper-content" type="danger" @click="findDelete">删除</el-button>
+      </el-col>
     </el-form>
   </div>
-  <div>
-    <el-button style="width: 50%;padding: 20px" type="primary" @click="update">确认</el-button>
-  </div>
-  <div>
-    <el-button style="width: 50%;margin-top:20px;padding: 20px" type="danger" @click="findDelete">删除</el-button>
-  </div>
 
-<<<<<<< HEAD
-  <Layer :layer="DeleteComfirmBox" @confirm="Delete" ref="layerDom" v-if="DeleteComfirmBox.show">
-=======
   <Layer :layer="DeleteConfirmBox" @confirm="Delete" ref="layerDom" v-if="DeleteConfirmBox.show">
->>>>>>> 82f255c3322240eef1727bc57e379024bf6dd09a
 
   </Layer>
 </template>
@@ -34,12 +50,7 @@ import type {Ref} from 'vue'
 import type {ElFormItemContext} from 'element-plus/lib/el-form/src/token'
 import {defineComponent, reactive, ref, watch} from 'vue'
 import {ElMessage} from 'element-plus'
-import {useStore} from 'vuex'
-<<<<<<< HEAD
-import {departmentUpdate} from '@/api/department'
-=======
-import {departmentUpdate,departmentDelete} from '@/api/department'
->>>>>>> 82f255c3322240eef1727bc57e379024bf6dd09a
+import {departmentUpdate, departmentDelete} from '@/api/department'
 import Layer from "@/components/layer/messagebox.vue";
 
 // const props = defineProps({
@@ -74,14 +85,18 @@ export default defineComponent({
     }
   },
   setup(props, ctx) {
+    const value_clockin = ref<[Date, Date]>([
+      new Date(2023, 1, 1, 0, 0),
+      new Date(2023, 1, 1, 23, 59),
+    ])
+    const value_clockout = ref<[Date, Date]>([
+      new Date(2023, 1, 1, 0, 0),
+      new Date(2023, 1, 1, 23, 59),
+    ])
     let data: any = reactive(props.dic)
     const ruleForm: Ref<ElFormItemContext | null> = ref(null)
     const layerDom: Ref<LayerType | null> = ref(null)
-<<<<<<< HEAD
-    const DeleteComfirmBox = reactive({
-=======
     const DeleteConfirmBox = reactive({
->>>>>>> 82f255c3322240eef1727bc57e379024bf6dd09a
       width: "30%",
       show: false,
       title: '',
@@ -91,8 +106,13 @@ export default defineComponent({
       id: data.id,
       name: data.name,
       notice: data.notice,
-      staff_count: data.staff_count
+      staff_count: data.staff_count,
+      clockin:([value_clockin.value[0].setTime(data.clock_in_start),value_clockin.value[1].setTime(data.clock_in_end)]),
+      clockout:([value_clockin.value[0].setTime(data.clock_out_start),value_clockin.value[1].setTime(data.clock_out_end)])
     })
+    console.log(value_clockin)
+    console.log(new Date().setTime(1675180800000))
+    console.log(form.clockin[0])
     watch(
         () => props.dic,
         (newValue, oldValue) => {
@@ -101,10 +121,18 @@ export default defineComponent({
           form.name = newValue.name
           form.notice = newValue.notice
           form.staff_count = newValue.staff_count
+          form.clockin=([value_clockin.value[0].setTime(newValue.clock_in_start),value_clockin.value[1].setTime(newValue.clock_in_end)])
+          form.clockout=([value_clockin.value[0].setTime(newValue.clock_out_start),value_clockin.value[1].setTime(newValue.clock_out_end)])
         }
     )
     const rules = {
       name: [{required: true, message: '请输入部门名', trigger: 'blur'}]
+    }
+
+    function TimePicker() {
+      console.log(value_clockin.value)
+      console.log(value_clockin.value[0].getTime())
+      console.log(typeof (value_clockin.value[0].getTime()))
     }
 
     function update() {
@@ -121,7 +149,11 @@ export default defineComponent({
               let params = {
                 id: form.id,
                 name: form.name,
-                notice: form.notice
+                notice: form.notice,
+                clock_in_start:form.clockin.value[0].getTime(),
+                clock_in_end:value_clockin.value[1].getTime(),
+                clock_out_start:value_clockout.value[0].getTime(),
+                clock_out_end:value_clockout.value[1].getTime(),
               }
               departmentUpdate(params)
                   .then(res => {
@@ -140,16 +172,6 @@ export default defineComponent({
       }
     }
 
-<<<<<<< HEAD
-    function Delete(){
-      if(data.staff_count!=0){
-        ElMessage({
-                type: 'warning',
-                message: '未作任何改变'
-              })
-        DeleteComfirmBox.show=false
-              return false;
-=======
     function Delete() {
 
       if (form.staff_count != 0) {
@@ -172,19 +194,13 @@ export default defineComponent({
               // layerDom.value && layerDom.value.close()
               location.reload()
             })
->>>>>>> 82f255c3322240eef1727bc57e379024bf6dd09a
       }
     }
 
 
     function findDelete() {
-<<<<<<< HEAD
-      DeleteComfirmBox.show = true
-      DeleteComfirmBox.title="该部门将被永久删除"
-=======
       DeleteConfirmBox.show = true
       DeleteConfirmBox.title = "该部门将被永久删除"
->>>>>>> 82f255c3322240eef1727bc57e379024bf6dd09a
     }
 
     return {
@@ -194,17 +210,29 @@ export default defineComponent({
       ruleForm,
       update,
       findDelete,
-<<<<<<< HEAD
-      DeleteComfirmBox,
-=======
+      Delete,
       DeleteConfirmBox,
->>>>>>> 82f255c3322240eef1727bc57e379024bf6dd09a
-      Delete
+      TimePicker
     }
   }
 })
 </script>
 
 <style lang="scss" scoped>
+.wrapper {
+  width: 100%;
+  height: auto;
+  display: flow;
+  justify-content: space-between;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
 
+.wrapper-content {
+  width: 30%;
+  padding: 20px;
+  margin-left: 10%;
+  margin-right: 10%;
+  margin-top: 5%;
+}
 </style>
