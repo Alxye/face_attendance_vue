af0262dbcd5b146d34201b6f61761e97b97379a1
Xixi <944652593zxx@gmail.com>
2023-03-17 23:26:34 +0800

    yes

---
+++ dist/assets/loading.d66bf6fc.js
@@ -0,0 +1,1 @@
+import{I as t}from"./index.abef33e9.js";let e=0,o;const s=()=>{e===0&&(o=t.service({fullscreen:!0,lock:!0,text:"\u52A0\u8F7D\u4E2D...",background:"rgba(0, 0, 0, 0.3)"})),e++},a=()=>{e<=0||(e--,e===0&&o.close())};export{a as h,s};
--- vite.config.ts
+++ vite.config.ts
@@ -6,88 +6,101 @@
  * @FilePath: \vue-admin-box\vite.config.ts
  * @Description: 
  */
-import { ConfigEnv, UserConfigExport } from 'vite'
+import {ConfigEnv, UserConfigExport} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { viteMockServe } from 'vite-plugin-mock'
+import {viteMockServe} from 'vite-plugin-mock'
 import {vitePluginSvg} from "@webxrd/vite-plugin-svg"
-import { resolve } from 'path'
+import {resolve} from 'path'
 
 const pathResolve = (dir: string): any => {
-  return resolve(__dirname, ".", dir)
+    return resolve(__dirname, ".", dir)
 }
 
 const alias: Record<string, string> = {
-  '@': pathResolve("src")
+    '@': pathResolve("src")
 }
 
-/** 
+/**
  * @description-en vite document address
  * @description-cn vite官网
  * https://vitejs.cn/config/ */
-export default ({ command }: ConfigEnv): UserConfigExport => {
-  const prodMock = true;
-  return {
-    base: './',
-    resolve: {
-      alias
-    },
-    server: {
-      port: 3001,
-      host: '0.0.0.0',
-      // port: 5000,
-      // host: '101.132.152.202',
-      open: true,
-      // proxy: { // 代理配置
-      //   '/dev': 'https://www.fastmock.site/mock/48cab8545e64d93ff9ba66a87ad04f6b/'
-      // },
-    },
-    build: {
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            'echarts': ['echarts']
-          }
-        }
-      }
-    },
-    plugins: [
-      vue(),
-      // viteMockServe({
-      //   mockPath: 'mock',
-      //   localEnabled: command === 'serve',
-      //   prodEnabled: command !== 'serve' && prodMock,
-      //   watchFiles: true,
-      //   injectCode: `
-      //     import { setupProdMockServer } from '../mockProdServer';
-      //     setupProdMockServer();
-      //   `,
-      //   logger: true,
-      // }),
-      vitePluginSvg({
-        // 必要的。必须是绝对路径组成的数组。
-        iconDirs: [
-            resolve(__dirname, 'src/assets/svg'),
-        ],
-        // 必要的。入口script
-        main: resolve(__dirname, 'src/main.js'),
-        symbolIdFormat: 'icon-[name]'
-      }),
-    ],
-    css: {
-      postcss: {
-        plugins: [
-            {
-              postcssPlugin: 'internal:charset-removal',
-              AtRule: {
-                charset: (atRule) => {
-                  if (atRule.name === 'charset') {
-                    atRule.remove();
-                  }
+export default ({command}: ConfigEnv): UserConfigExport => {
+    const prodMock = true;
+    return {
+        base: './',
+        resolve: {
+            alias
+        },
+        // devServer: {
+        //     host: '0.0.0.0',
+        //     disableHostCheck: true,
+        //     proxy: {
+        //         '/api': {
+        //             target: '0.0.0.0:5000',//后端接口地址，按自己的改
+        //             changeOrigin: true,//是否允许跨越
+        //             pathRewrite: {
+        //                 '^/api': ''
+        //             }
+        //         }
+        //     }
+        // },
+        server: {
+            port: 3001,
+            host: '0.0.0.0',
+            // port: 5000,
+            // host: '101.132.152.202',
+            open: true,
+            // proxy: { // 代理配置
+            //   '/dev': 'https://www.fastmock.site/mock/48cab8545e64d93ff9ba66a87ad04f6b/'
+            // },
+        },
+        build: {
+            rollupOptions: {
+                output: {
+                    manualChunks: {
+                        'echarts': ['echarts']
+                    }
                 }
-              }
             }
+        },
+        plugins: [
+            vue(),
+            // viteMockServe({
+            //   mockPath: 'mock',
+            //   localEnabled: command === 'serve',
+            //   prodEnabled: command !== 'serve' && prodMock,
+            //   watchFiles: true,
+            //   injectCode: `
+            //     import { setupProdMockServer } from '../mockProdServer';
+            //     setupProdMockServer();
+            //   `,
+            //   logger: true,
+            // }),
+            vitePluginSvg({
+                // 必要的。必须是绝对路径组成的数组。
+                iconDirs: [
+                    resolve(__dirname, 'src/assets/svg'),
+                ],
+                // 必要的。入口script
+                main: resolve(__dirname, 'src/main.js'),
+                symbolIdFormat: 'icon-[name]'
+            }),
         ],
-      },
-    }
-  };
+        css: {
+            postcss: {
+                plugins: [
+                    {
+                        postcssPlugin: 'internal:charset-removal',
+                        AtRule: {
+                            charset: (atRule) => {
+                                if (atRule.name === 'charset') {
+                                    atRule.remove();
+                                }
+                            }
+                        }
+                    }
+                ],
+            },
+        }
+    };
 }
